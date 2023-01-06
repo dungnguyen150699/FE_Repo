17c17d1f1e2cc42dfbdfaae367cdafa5655ab484
Nguyễn Tiến Dũng <nguyentiendung@techasians.com>
2023-01-06 18:15:21 +0700

    add react ts

---
+++ ts-project-first/src/App.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+// let name:String = "NTD";
+// let age:number = 20;
+// let sleep:boolean=false;
+// let arr:string[] = ['project1','project2'];
+// arr[0] = '12';
+// let typer:any = 'any type';
+/**
+ * C1 
+    type Student = {
+      name : String,
+      age : number
+    };
+    let student : Student ={
+      name : 'dungnt',
+      age : 21
+    };
+
+    C2
+    let student : {
+      name : string,
+      age : number
+    } = {
+      name : 'dungnt',
+      age : number
+    }
+*/
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
