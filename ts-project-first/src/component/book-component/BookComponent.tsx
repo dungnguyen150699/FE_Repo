258b79881552a9714ff9fb55ebcc7bc6727a868d
Dũng <dungnguyen150699@gmail.com>
2023-01-29 21:54:10 +0700

    add init htm_css

--- ts-project-first/src/component/book-component/BookComponent.tsx
+++ ts-project-first/src/component/book-component/BookComponent.tsx
@@ -5,6 +5,7 @@ import { styled } from '@mui/material/styles';
 import * as React from 'react';
 import fbLogo from '../../img/fb-logo.png';
 import bookImg from '../../img/book.jpg';
+import './book.css';
 
 export default function BookComponent(){
     const Item = styled(Paper)(({ theme }) => ({
@@ -16,18 +17,17 @@ export default function BookComponent(){
       }));
       
     return (
-        <Container maxWidth="xl" style={{marginTop:"50px"}}>
+        <Container className="container--book" maxWidth="xl" style={{marginTop:"50px"}}>
             <Grid container spacing={2}>
-                {
-                    
-                }
-                <Grid item xs={3}>
+                <Grid item xs={3}
+                className="grid">
                     <img src={bookImg} style={
                         {
                             width: '235px',
                             height: '220px'
                         }
-                } ></img>
+                    } ></img>
+                    <button className="btn-addcart">Add Cart</button>
                 </Grid>
                 <Grid item xs={3}>
                     <img src={bookImg} style={
